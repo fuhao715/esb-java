8e5244fb7f41c49fa6c24fca1836e9ae89bc36dd
fuhao715 <fuhao715@gmail.com>
2013-12-16 16:43:33 +0800

    服务注册测试

--- test/src/com/fuhao/esb/test/annotation/TestCallService.java
+++ test/src/com/fuhao/esb/test/annotation/TestCallService.java
@@ -16,6 +16,6 @@ public class TestCallService extends ServiceTestCase {
     public void testCall() throws ESBBaseCheckedException {
         System.out.println("----11111111111111111----");
         String str = (String)ESBServiceUtils.callService("esb_insertPO","fuhao");
-        System.out.println("----2222222222222222----");
+        System.out.println("----2222222222222222----"+str);
     }
 }
