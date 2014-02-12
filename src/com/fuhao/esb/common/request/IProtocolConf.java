fbd6287042de98592d97d2f907dfd29029949c8c
fuhao715 <fuhao715@gmail.com>
2014-02-12 11:30:33 +0800

    增加协议类型枚举项

--- src/com/fuhao/esb/common/request/IProtocolConf.java
+++ src/com/fuhao/esb/common/request/IProtocolConf.java
@@ -16,11 +16,11 @@ public interface IProtocolConf {
      * 交易的通讯协议类型
      */
     public static enum ProtocolType { // 支持协议类型
-        LOCAL, EJB, JMS, EJB_BEAN, EJB_CONTAINER, JMS_BEAN, JMS_CONTAINER ,WEBSERVICE,HESSIAN,ACTIVEMQ,IBMMQ,RABBITMQ,SOCKET,FTP,LocalXML
+        LOCAL, EJB, JMS, EJB_BEAN, EJB_CONTAINER, JMS_BEAN, JMS_CONTAINER ,WEBSERVICE,HESSIAN,ACTIVEMQ,IBMMQ,RABBITMQ,SOCKET,FTP,LocalXML,DYNAMIC,EXTHANG
     }
 
     public static enum synProtocolType{ // 异步协议类型
-        JMS, JMS_BEAN, JMS_CONTAINER , ACTIVEMQ, IBMMQ, RABBITMQ;
+        JMS, JMS_BEAN, JMS_CONTAINER , ACTIVEMQ, IBMMQ, RABBITMQ,DYNAMIC,EXTHANG;
         public static List<String> getEnumNames() {
             return CollectionUtils.getValuesNames(Arrays.asList(values()));
         }
